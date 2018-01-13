c1fd9d636548cd849cdc14b3ee022ea37002bb61
Tobias Kunze <rixx@cutebit.de>
2018-01-13 12:55:43 +0100

    Fix app config

--- byro_shackspace/__init__.py
+++ byro_shackspace/__init__.py
@@ -2,4 +2,14 @@ from django.apps import AppConfig
 
 
 class ShackspacePluginConfig(AppConfig):
-    name = 'byro.plugins.shackspace'
+    name = 'byro_shackspace'
+
+    class ShackspacePluginMeta:
+        name = 'shackspace'
+
+    def ready(self):
+        print('fooooo')
+        from . import utils
+
+
+default_app_config = 'byro_shackspace.ShackspacePluginConfig'
